295f2e6ddf57bb6e6d82f9b63e94ddcd4b8b7e85
mtiowl <noemail@junk.net>
2014-11-14 13:22:37 -0500

    HTML Reports
    Test scriptsto set up CSS

---
+++ HTML Reports/set_markup2.sql
@@ -0,0 +1,53 @@
+set markup HTML ON HEAD "<style type='text/css'> -
+body { -
+font:10pt Arial,Helvetica,sans-serif; -
+color:blue; background:white; } -
+p { -
+font:8pt Arial,sans-serif; -
+color:grey; background:white; } -
+table { -
+font:10pt Arial,Helvetica,sans-serif; -
+text-align:right; -
+color:Black; background:white; -
+padding:0px 0px 0px 0px; margin:0px 0px 0px 0px; } -
+tr:nth-child(2n) { background-color: #CCE6FF ;} -
+tr:nth-child(2n+1) { background-color: LightGray;} -
+th { -
+font:bold 10pt Arial,Helvetica,sans-serif; -
+color:#336699; -
+background:#cccc99; -
+padding:0px 0px 0px 0px;} -
+h1 { -
+font:16pt Arial,Helvetica,Geneva,sans-serif; -
+color:#336699; -
+background-color:White; -
+border-bottom:1px solid #cccc99; -
+margin-top:0pt; margin-bottom:0pt; padding:0px 0px 0px 0px;} -
+h2 { -
+font:bold 10pt Arial,Helvetica,Geneva,sans-serif; -
+color:#336699; -
+background-color:White; -
+margin-top:4pt; margin-bottom:0pt;} -
+a { -
+font:9pt Arial,Helvetica,sans-serif; -
+color:#663300; -
+background:#ffffff; -
+margin-top:0pt; margin-bottom:0pt; vertical-align:top;} -
+.threshold-critical { -
+font:bold 10pt Arial,Helvetica,sans-serif; -
+color:red; } -
+.threshold-warning { -
+font:bold 10pt Arial,Helvetica,sans-serif; -
+color:orange; } -
+.row:nth-child(even) { -
+  background: #dde; } -
+.row:nth-child(odd) { -
+  background: white;} -
+.threshold-ok { -
+font:bold 10pt Arial,Helvetica,sans-serif; -
+color:green; } -
+</style> -
+<title>SQL*Plus Report</title>" -
+BODY "<img src=http://www.acme.com/company_logo..gif/>" -
+TABLE "border='1' width='90%' align='center'" -
+ENTMAP OFF SPOOL ON
